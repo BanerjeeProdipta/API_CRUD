2006f0b0b3e956058c55df06aaf6a77e9419501e
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2021-07-22 16:52:52 +0600

    post

--- post.js
+++ post.js
@@ -1,39 +1,36 @@
 const AWS = require('aws-sdk');
+
 const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-exports.handler = async(event) => {
-    let body;
-    let statusCode = 200;
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
 
-    try {
-        let params = {
-            TableName: 'todo-application',
-            Item: {
-                id: event.id,
-                completionStatus: event.completionStatus,
-                title: event.title,
-                body: event.body,
-            },
-        };
-
-        // Using await, make sure object writes to DynamoDB table before continuing execution
-        await documentClient
-            .put(params)
-            .promise()
-            .then(() => {
-                body = {
-                    Operation: 'POST',
-                    Message: 'SUCCESS',
-                    Item: event,
-                };
-            });
-    } catch (err) {
-        statusCode = 400;
-        body = err.message;
-    }
-
-    return {
-        statusCode,
-        body,
+  try {
+    let params = {
+      TableName: 'todo-application',
+      Item: {
+        id: context.awsRequestId,
+        completionStatus: false,
+        title: event.title,
+        body: event.body,
+      },
     };
+
+    // Using await, make sure object writes to DynamoDB table before continuing execution
+    await documentClient.put(params).promise();
+    body = {
+      Operation: 'POST',
+      Message: 'SUCCESS',
+      Item: params.Item,
+    };
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  }
+
+  return {
+    statusCode,
+    body,
+  };
 };
