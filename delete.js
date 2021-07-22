f4b41ac632c6f87f9690872b09e7cb6b7d88d9d9
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2021-07-22 20:08:50 +0600

    format

--- delete.js
+++ delete.js
@@ -1,26 +1,26 @@
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-exports.handler = async(event) => {
-    let body;
-    let statusCode = 200;
+exports.handler = async (event) => {
+  let body;
+  let statusCode = 200;
 
-    try {
-        let params = {
-            TableName: 'todo-application',
-            Key: {
-                id: event.pathParameters.id,
-            },
-        };
-        await documentClient.delete(params).promise();
-        body = `Deleted item ${event.pathParameters.id}`;
-    } catch (err) {
-        statusCode = 400;
-        body = err.message;
-    }
-
-    return {
-        statusCode,
-        body,
+  try {
+    let params = {
+      TableName: 'todo-application',
+      Key: {
+        id: event.pathParameters.id,
+      },
     };
+    await documentClient.delete(params).promise();
+    body = `Deleted item ${event.pathParameters.id}`;
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  }
+
+  return {
+    statusCode,
+    body,
+  };
 };
